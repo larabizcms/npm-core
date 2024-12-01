897c89de13cad77e3d82ead057134457fe24fbdc
Thế Anh Đặng <dangtheanh16@gmail.com>
2024-12-01 10:28:26 +0700

    Remove redux

--- src/features/auth/index.ts
+++
@@ -1,5 +0,0 @@
-import * as authAction from './authActions';
-import * as authService from './authService';
-import * as authSlice from './authSlice';
-
-export { authAction, authService, authSlice };
